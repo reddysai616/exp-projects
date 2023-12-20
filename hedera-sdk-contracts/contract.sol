c6a9c20444e7c0a880c2a181905c0db65420d781
reddysai616 <62210706+reddysai616@users.noreply.github.com>
2023-12-20 21:15:15 +0530

    hedera-sdk

---
+++ hedera-sdk-contracts/contract.sol
@@ -0,0 +1,12 @@
+// Sample Smart Contract 1
+contract SampleContract1 {
+    string public message;
+
+    constructor(string memory initialMessage) {
+        message = initialMessage;
+    }
+
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+}
